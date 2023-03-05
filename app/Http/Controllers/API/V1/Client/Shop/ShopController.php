55e5dc72e5fe663c92f3b5091a1db85ace44a084
Soft IT Care <117034691+softitcaredev@users.noreply.github.com>
2023-03-05 19:11:58 +0600

    Update ShopController.php

--- app/Http/Controllers/API/V1/Client/Shop/ShopController.php
+++ app/Http/Controllers/API/V1/Client/Shop/ShopController.php
@@ -13,7 +13,7 @@ use Illuminate\Validation\ValidationException;
 class ShopController extends MerchantBaseController
 {
     use sendApiResponse;
-
+    
     public function index(Request $request): JsonResponse
     {
         if ($request->header('domain') && $request->header('domain') !== null) {
@@ -21,6 +21,34 @@ class ShopController extends MerchantBaseController
             if (!$shop) {
                 throw ValidationException::withMessages([
                     'shop_id' => 'domain not found'
+                ]);
+            }
+
+            $activeTheme = ActiveTheme::query()->join('themes', 'themes.id', '=', 'active_themes.theme_id')
+                ->where('active_themes.shop_id', $shop->shop_id)
+                ->where('active_themes.type', 'multiple')
+                ->first();
+
+            $shop['theme_id'] = NULL;
+            if ($activeTheme) {
+                $shop['theme_id'] = $activeTheme->name;
+            }
+
+            $shop->load('shop_logo');
+
+            return $this->sendApiResponse($shop);
+        }
+
+        return $this->sendApiResponse('', 'domain not found', 'DomainNotFound', [], 401);
+    }
+    public function domain(Request $request): JsonResponse
+    {
+        if ($request->header('domain') && $request->header('domain') !== null) {
+            $shop = Shop::query()->where('domain_request', $request->header('domain'))->first();
+            //dd($shop);
+            if (!$shop) {
+                throw ValidationException::withMessages([
+                    'domain_request' => 'domain not found'
                 ]);
             }
 
