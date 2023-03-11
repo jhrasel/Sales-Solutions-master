73b662655ac6c54f88875c9ff4a3de88ff577879
Soft IT Care <117034691+softitcaredev@users.noreply.github.com>
2023-03-11 13:20:33 +0600

    Update PageController.php

--- app/Http/Controllers/API/V1/Client/Page/PageController.php
+++ app/Http/Controllers/API/V1/Client/Page/PageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PageRequest;
 use App\Models\ActiveTheme;
 use App\Models\Page;
+use App\Models\Media;
 use App\Traits\sendApiResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
