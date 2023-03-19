854aa326c61124fa681b8ce8ffb6feb67800019e
masumrahmanhasan <masumrahmanhasan@gmail.com>
2023-03-19 15:44:31 +0600

    product name buf fix on order resource

--- app/Http/Resources/OrderDetailsResource.php
+++ app/Http/Resources/OrderDetailsResource.php
@@ -2,24 +2,26 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
 
 class OrderDetailsResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'product'=> $this->product->product_name,
+            'product'=> $this->product->product_name ?? null,
             'price'=> $this->product->price,
             'quantity'=> $this->product_qty,
-            'product'=> $this->product->product_name,
         ];
     }
 }
